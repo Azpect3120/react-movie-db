2f649dca3f3c3b6bcd862836b9b8637cdaf27965
Azpect3120 <hhargreaves2006@gmail.com>
2023-10-08 17:19:37 -0700

    FIX: Resized images to allow for more room to load images and prevents the funky load behavior

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -5,11 +5,11 @@ interface Props {
   movie: Movie
 }
 
-const IMAGE_URL_PREFIX = "https://image.tmdb.org/t/p/w200"
+const IMAGE_URL_PREFIX = "https://image.tmdb.org/t/p/w300"
 
 function MovieCard (props: Props): JSX.Element {
   return (
-    <div className="flex flex-col items-center mb-4 w-[220px]">
+    <div className="flex flex-col items-center mb-4 w-[320px]">
       <p className="absolute bg-dark-primary text-white w-10 py-0.5 rounded-lg text-center"> {props.movie.vote_average} </p>
       <img src={IMAGE_URL_PREFIX + props.movie.poster_path} alt="Movie poster" className="mt-2.5 rounded-md" />
       <h1 className="w-full px-[12.5px] py-2 text-md text-dark-text-base"> { props.movie.title } </h1>
